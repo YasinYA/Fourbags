712b824dacacb6649bd40111e3a5cd8a5d43008a
Yasin Yusuf <yaasiinyuusuf78@gmail.com>
2016-10-23 07:46:31 -0400

    created order model

--- apparel/admin.py
+++ apparel/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Item, Size, ItemImage
+from .models import Item, Size, ItemImage, Order
 
 admin.site.register(Item)
 admin.site.register(Size)
 admin.site.register(ItemImage)
+admin.site.register(Order)
 
